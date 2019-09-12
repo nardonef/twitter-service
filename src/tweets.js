f24033fdbc9df248a9ec857d6ccfdf8523e3ac98
Frank 'The Tank' Nardone <frank.nardone@nfrastructure.com>
2019-09-11 21:10:26 -0400

    inital commit

---
+++ src/tweets.js
@@ -0,0 +1,14 @@
+const getTweets = async (event) => {
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: 'Go Serverless v1.0! Your function executed successfully!',
+            input: event,
+        }, null, 2),
+    };
+};
+
+
+
+
+exports.handler = getTweets;
